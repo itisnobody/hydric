4ad8f883ce50ef3025bf2356d1887d123b7c4cc3
itisnobody <alexandr.goryachev@gmail.com>
2019-08-09 01:11:34 +0300

    Animation fixed, dependencies updated (including Galp 4)

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-var gulp = require('gulp'),
-    clean = require('gulp-clean'),
+var { src, dest, series, parallel, watch } = require('gulp'),
+    del = require('del'),
+    babel = require('gulp-babel'),
     sass = require('gulp-sass'),
     prefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
     sourcemaps = require('gulp-sourcemaps'),
-    runSequence = require('run-sequence'),
 	ghPages = require('gulp-gh-pages');
-    browserSync = require('browser-sync').create(),
-    reload = browserSync.reload;
 
-// Pathes
 var path = {
     dev: {
         clean: ['./dev', './.publish'],
@@ -23,87 +22,65 @@ var path = {
         html: 'src/*.html',
         fonts: 'src/fonts/**/*.*',
         img: 'src/img/**/*.*',
-        style: 'src/style/**/*.css',
-        sass: ['src/style/**/*.scss', 'src/style/**/*.sass']
+        sass: 'src/style/**/*.scss'
     }
 };
 
-// Clean
-gulp.task('clean', function () {
-    return gulp.src(path.dev.clean, {read: false})
-        .pipe(clean());
-});
+function clean() {
+  return del(path.dev.clean)
+}
 
-// Server
-gulp.task('server', function () {
+function server() {
     browserSync.init({
         server: path.dev.html,
         port: 3001
     });
-    gulp.watch(path.src.html, ['html']);
-    gulp.watch(path.src.sass, ['sass']);
-    gulp.watch(path.src.style, ['css']);
-});
+    watch(path.src.html, 'html');
+    watch(path.src.sass, 'styles');
+}
 
-// Copy fonts
-gulp.task('copy', function() {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.dev.fonts))
-});
+function fonts() {
+    return src(path.src.fonts)
+        .pipe(dest(path.dev.fonts))
+}
 
-//HTML
-gulp.task('html', function () {
-    gulp.src(path.src.html)
-        .pipe(gulp.dest(path.dev.html))
-        .pipe(reload({stream: true}));
-});
+function html() {
+    return src(path.src.html)
+        .pipe(dest(path.dev.html))
+}
 
-//Images
-gulp.task('images', function () {
-    gulp.src(path.src.img)
-        // .pipe(imagemin({
-        //     progressive: true,
-            // svgoPlugins: [{removeViewBox: false}],
-        //     use: [pngquant()],
-        //     interlaced: true
-        // }))
-        .pipe(gulp.dest(path.dev.img))
-        .pipe(reload({stream: true}));
-});
+function images() {
+  return src(path.src.img)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()],
+      interlaced: true
+    }))
+    .pipe(dest(path.dev.img))
+}
 
-// Sass/Scss to CSS
-gulp.task('sass', function () {
-    gulp.src(path.src.sass)
-        .pipe(sourcemaps.init())
-	        .pipe(sass())
-	        .pipe(prefixer())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.dev.style))
-        .pipe(reload({stream: true}));
-});
+function styles() {
+  return src(path.src.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(prefixer())
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(path.dev.style))
+}
 
-// Style
-gulp.task('css', function () {
-    gulp.src(path.src.style)
-        .pipe(prefixer())
-        .pipe(gulp.dest(path.dev.style))
-        .pipe(reload({stream: true}));
-});
+exports.clean = clean;
+exports.server = server;
+exports.images = images;
+exports.deploy = deploy;
 
+exports.default = series(clean, fonts, parallel(html, styles, images));
 
-// ---------------------------------------------------------------------
-// | Main tasks                                                        |
-// ---------------------------------------------------------------------
-
-
-gulp.task('default', function(callback) {
-    runSequence('copy',
-        ['html', 'images', 'css', 'sass'],
-        'server',
-        callback);
-});
-
-gulp.task('deploy', function() {
-  return gulp.src('./dev/**/*')
+function deploy() {
+  return src('./dev/**/*')
     .pipe(ghPages());
-});
+}
